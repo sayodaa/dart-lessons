416a26aff2005a40ec4ff43514905203abc05bb6
sayodaa <sayedmatger@gmail.com>
2025-08-19 14:15:04 +0300

    premitive data types

---
+++ lesson1.dart
@@ -0,0 +1,35 @@
+void main() {
+  // أنا بعرف الاسم
+  String name = 'Mohammed';
+  // انا بعرف العمر
+  int age = 24;
+  int salary = 24;
+  dynamic test0 =2.2;
+  var test22 =25; 
+  num test3;
+  dynamic test1 = 'sayed';
+  dynamic test2 = 12.5;
+  const double pi = 3.14;
+  bool isValid = false;
+
+  print('$name teach programing');
+  print('$name age is $age');
+  print('$name age is \$$salary');
+  print('pi is $pi');
+  print(isValid);
+  print(test22);
+  print(test0);
+  print(test1);
+  print(test2);
+  
+
+  name = 'Sayed';
+  age = 25;
+  isValid = true;
+  print('*******************');
+
+  print('$name teach programing');
+  print('$name age is $age');
+  print('pi is $pi');
+  print(isValid);
+}
